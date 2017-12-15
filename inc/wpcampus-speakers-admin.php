9a35695fc6db901bab93ac07845b9ea8093b2b5d
Rachel Carden <Rachel.Carden@disney.com>
2017-12-14 23:15:21 -0800

    Adding speakers section to the admin menu

--- inc/wpcampus-speakers-admin.php
+++ inc/wpcampus-speakers-admin.php
@@ -14,7 +14,32 @@ class WPCampus_Speakers_Admin {
 	 * @access  public
 	 */
 	public function __construct() {}
-}
 
-// Let's get this party started.
-new WPCampus_Speakers_Admin();
+	/**
+	 * Registers all of our hooks and what not.
+	 */
+	public static function register() {
+		$plugin = new self();
+
+		// Add items to the admin menu.
+		add_action( 'admin_menu', array( $plugin, 'add_menu_pages' ) );
+
+	}
+
+	/**
+	 * Add pages to the admin.
+	 */
+	public function add_menu_pages() {
+		$plugin = new self();
+
+		// Add menu section to manage all of the speaker information.
+		add_menu_page( __( 'Speakers', 'wpcampus' ), __( 'Speakers', 'wpcampus' ), 'manage_wpc_speakers', 'wpc-speakers', array( $plugin, 'print_speakers_main_page' ), 'dashicons-megaphone', 10.111 );
+
+	}
+
+	/**
+	 * Print the main page of the speakers section.
+	 */
+	public function print_speakers_main_page() {}
+}
+WPCampus_Speakers_Admin::register();

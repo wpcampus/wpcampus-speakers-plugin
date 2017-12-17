5f08f8e67011f1e4c36a64e698bf481c702cc0c0
Rachel Cherry <bamadesigner@gmail.com>
2017-12-17 00:46:39 -0800

    Added Speaker Proposal(s) meta box to profile edit screen

--- inc/wpcampus-speakers-admin.php
+++ inc/wpcampus-speakers-admin.php
@@ -34,6 +34,9 @@ class WPCampus_Speakers_Admin {
 		// Adds dropdown to filter the speaker status.
 		add_action( 'restrict_manage_posts', array( $plugin, 'add_proposal_filters' ), 100 );
 
+		// Add meta boxes.
+		add_action( 'add_meta_boxes', array( $plugin, 'add_meta_boxes' ), 1, 2 );
+
 	}
 
 	/**
@@ -219,6 +222,122 @@ class WPCampus_Speakers_Admin {
 
 				break;
 		}
+	}
+
+	/**
+	 * Add our custom admin meta boxes.
+	 */
+	public function add_meta_boxes( $post_type, $post ) {
+
+		switch ( $post_type ) {
+
+			case 'profile':
+
+				add_meta_box(
+					'wpcampus-speakers-proposal',
+					__( 'Speaker Proposal(s)', 'wpcampus' ),
+					array( $this, 'print_meta_boxes' ),
+					$post_type,
+					'normal',
+					'high'
+				);
+				break;
+		}
+	}
+
+	/**
+	 * Prints the content in our custom admin meta boxes.
+	 */
+	public function print_meta_boxes( $post, $metabox ) {
+
+		switch ( $metabox['id'] ) {
+
+			case 'wpcampus-speakers-proposal':
+				$this->print_speaker_proposals_table( $post->ID );
+				break;
+		}
+	}
+
+	/**
+	 * Print a table of info for a speaker's proposal(s).
+	 */
+	public function print_speaker_proposals_table( $speaker_id ) {
+		global $wpdb;
+
+		$proposals = $wpdb->get_results( $wpdb->prepare( "SELECT ID, post_title,
+			proposalm2.meta_value AS proposal_status,
+			eventm.meta_value AS event_id, eventt.name AS event_name, eventt.slug AS event_slug
+			FROM {$wpdb->posts} proposal
+			INNER JOIN {$wpdb->postmeta} proposalm1 ON proposalm1.post_id = proposal.ID AND proposalm1.meta_key REGEXP '^speakers\_([0-9]+)\_speaker$' AND proposalm1.meta_value = %s
+			LEFT JOIN {$wpdb->postmeta} proposalm2 ON proposalm2.post_id = proposal.ID AND proposalm2.meta_key = 'proposal_status'
+			LEFT JOIN {$wpdb->postmeta} eventm ON eventm.post_id = proposal.ID AND eventm.meta_key = 'event'
+			LEFT JOIN {$wpdb->terms} eventt ON eventt.term_id = eventm.meta_value
+			WHERE proposal.post_type = 'proposal'", $speaker_id
+		));
+
+		if ( empty( $proposals ) ) :
+			?><p><em><?php _e( 'This speaker has no proposals.', 'wpcampus' ); ?></em></p><?php
+			return;
+		endif;
+
+		?>
+		<style type="text/css">
+			table.wpc-speaker-proposals {
+				width: 100%;
+				text-align: left;
+				margin: 0;
+				border: 0;
+				padding: 0;
+			}
+			table.wpc-speaker-proposals th,
+			table.wpc-speaker-proposals td {
+				padding: 0 10px 0 0;
+			}
+		</style>
+		<table class="wpc-speaker-proposals">
+			<thead>
+				<tr>
+					<th><?php _e( 'Title', 'wpcampus' ); ?></th>
+					<th><?php _e( 'Status', 'wpcampus' ); ?></th>
+					<th><?php _e( 'Event', 'wpcampus' ); ?></th>
+					<th><?php _e( 'Speaker', 'wpcampus' ); ?>(s)</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+
+				foreach ( $proposals as $proposal ) :
+					?>
+					<tr>
+						<td><a href="<?php echo get_edit_post_link( $proposal->ID ); ?>"><?php echo $proposal->post_title; ?></a></td>
+						<td><?php $this->print_proposal_status_label( $proposal->proposal_status ); ?></td>
+						<td><?php
+
+						if ( ! empty( $proposal->event_name ) ) :
+
+							if ( ! empty( $proposal->event_slug ) ) :
+
+								$filter_url = add_query_arg( array(
+									'post_type' => 'proposal',
+									'event'     => $proposal->event_slug,
+								), admin_url( 'edit.php' ) );
+
+								?><a href="<?php echo $filter_url; ?>"><?php echo $proposal->event_name; ?></a><?php
+							else :
+								echo $proposal->event_name;
+							endif;
+						endif;
+
+						?></td>
+						<td><?php $this->print_proposal_speaker_list( $proposal->ID ); ?></td>
+					</tr>
+					<?php
+				endforeach;
+
+				?>
+			</tbody>
+		</table>
+		<?php
 	}
 
 	/**
